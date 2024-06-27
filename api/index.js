64613e44e39aa1ccc4162e9c47279f284c8ff7ea
Upayan Mazumder <upayanm3@gmail.com>
2024-06-27 11:27:22 +0530

    fetches alr

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const apiKey = process.env.API_KEY;
 const apiUrl = 'https://panel.eranodes.com/api/application/servers';
+const nodesApiUrl = 'https://panel.eranodes.com/api/application/nodes';
 const discordBotToken = process.env.DISCORD_BOT_TOKEN;
 const discordGuildId = process.env.DISCORD_GUILD_ID;
 
@@ -26,6 +27,34 @@ async function fetchServerData() {
     console.log(`Number of servers running: ${numberOfServers}`);
   } catch (error) {
     console.error('Error fetching server information:', error);
+  }
+}
+
+async function fetchNodeData() {
+  try {
+    const response = await fetch(nodesApiUrl, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const nodes = data.data;
+    let totalAllocatedRamMB = 0;
+
+    nodes.forEach(node => {
+      totalAllocatedRamMB += node.attributes.allocated_resources.memory;
+    });
+
+    const totalAllocatedRamGB = totalAllocatedRamMB / 1024;
+    console.log(`Total allocated RAM across all nodes: ${totalAllocatedRamGB.toFixed(2)} GB`);
+  } catch (error) {
+    console.error('Error fetching node information:', error);
   }
 }
 
@@ -52,4 +81,5 @@ async function fetchDiscordMemberCount() {
 }
 
 fetchServerData();
+fetchNodeData();
 fetchDiscordMemberCount();
