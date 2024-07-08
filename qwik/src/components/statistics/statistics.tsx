144a9046827aa9ef7bd430ad5404680f212bdd09
upayanmazumder <upayanm3@gmail.com>
2024-07-08 18:37:56 +0530

    🛠️ statistics.module.css -> Updated styles for section, heading, and detail 🛠️ statistics.tsx -> Updated useServerStats function and component to handle API accessibility

--- qwik/src/components/statistics/statistics.tsx
+++ qwik/src/components/statistics/statistics.tsx
@@ -3,14 +3,13 @@ import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import styles from './statistics.module.css';
 
-// Interface for the new data structure
 interface ServerStats {
   pterodactylServerCount: number;
   pterodactylTotalRam: string;
   discordMemberCount: number;
 }
 
-// Fetch data from the new API endpoint
+// Fetch data from the API
 const fetchDataFromAPI = async (): Promise<ServerStats | undefined> => {
   try {
     const response = await fetch('https://statsserverapi.eranodes.com/', {
@@ -25,35 +24,29 @@ const fetchDataFromAPI = async (): Promise<ServerStats | undefined> => {
     return responseData as ServerStats;
   } catch (error) {
     console.error('Error fetching server stats:', error);
-    return undefined; // Ensure that we return undefined in case of error
+    return undefined;
   }
 };
 
 // Route loader to fetch server stats data
-export const useServerStats = routeLoader$<ServerStats>(async () => {
-  const data = await fetchDataFromAPI();
-  if (data === undefined) {
-    // Handle the error case by returning default values
-    return {
-      pterodactylServerCount: 0,
-      pterodactylTotalRam: '0',
-      discordMemberCount: 0,
-    };
-  }
-  return data;
+export const useServerStats = routeLoader$<ServerStats | undefined>(async () => {
+  return await fetchDataFromAPI();
 });
 
 export default component$(() => {
   const serverStats = useServerStats();
 
+  // Hide the component when the API is inaccessible
+  if (!serverStats.value) {
+    return null;
+  }
+
   return (
-    <div class="container container-center">
-      <div class={styles.section}>
-        <p class={styles.heading}>Server Statistics</p>
-        <p class={styles.detail}>Pterodactyl Server Count: {serverStats.value.pterodactylServerCount}</p>
-        <p class={styles.detail}>Pterodactyl Total RAM: {serverStats.value.pterodactylTotalRam} GB</p>
-        <p class={styles.detail}>Discord Member Count: {serverStats.value.discordMemberCount}</p>
-      </div>
+    <div class={styles.section}>
+      <p class={styles.heading}>Our progress so far..</p>
+      <p class={styles.detail}>Servers hosted : {serverStats.value.pterodactylServerCount}</p>
+      <p class={styles.detail}>Total allocated RAM : {serverStats.value.pterodactylTotalRam} GB</p>
+      <p class={styles.detail}>Discord members : {serverStats.value.discordMemberCount}</p>
     </div>
   );
 });
