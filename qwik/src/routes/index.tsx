2506fcf0439b6824d1dde8ba71944332fccc38b1
upayanmazumder <upayanm3@gmail.com>
2024-07-09 06:34:41 +0530

    🛠️ global.css -> Added light and dark red colors. 🛠️ index.tsx -> Added Plans component, modified ellipsis color. 🛠️ styles.css -> Changed colors and added box-shadow to buttons, modified gradients and colors for button-small and ellipsis.

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -3,6 +3,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Statistics from '../components/statistics/statistics';
 import Hero from "../components/hero/hero";
+import Plans from "../components/plans/plans"
 import Testimonials from "../components/testimonials/testimonials";
 import SecondaryFooter from "../components/secondary footer/secondary-footer";
 export default component$(() => {
@@ -13,7 +14,7 @@ export default component$(() => {
       <div id="plans" class="container"></div>
       <Plans />
       <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-aqua"></div>
+      <div role="presentation" class="ellipsis ellipsis-red"></div>
       <div  id="reviews" class="container"></div>
       <Testimonials/>
       <SecondaryFooter/>
