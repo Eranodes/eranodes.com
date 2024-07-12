37b93eecaa081fcbb024dc806b28f43d716d577e
upayanmazumder <upayanm3@gmail.com>
2024-07-12 15:58:03 +0530

    Added maps

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -1,12 +1,35 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Statistics from '../components/statistics/statistics';
 import Hero from "../components/hero/hero";
-import Plans from "../components/introplans/plans"
+import Plans from "../components/introplans/plans";
 import Reviews from "../components/reviews/reviews";
 import SecondaryFooter from "../components/secondary footer/secondary-footer";
+
+import leafletStyles from "../../node_modules/leaflet/dist/leaflet.css?inline";
+
+import { LeafletMap } from "~/components/leaflet-map";
+import type { LocationsProps } from "~/models/location";
+
 export default component$(() => {
+
+  useStyles$(leafletStyles);
+  const currentLocation = useSignal<LocationsProps>({
+    name: "Germany",
+    point: [51.1657, 10.4515],
+    /**
+     * Define rectangle with: Southwest lat, South West Lng, North East lat,  North East lng points.
+     * Very interesting when use to filter in OpenStreetMap API to take POIs
+     * Example: https://qwik-osm-poc.netlify.app/
+     */
+    boundaryBox:
+      "43.14658914559456,-2.4765586853027344,43.202923523094725,-2.3467826843261723",
+    zoom: 2,
+    marker: true,
+  },
+)
+
   return (
     <>
       <Hero />
@@ -16,8 +39,11 @@ export default component$(() => {
       <div role="presentation" class="ellipsis"></div>
       <div role="presentation" class="ellipsis ellipsis-red"></div>
       <div  id="reviews" class="container"></div>
-      <Reviews/>
-      <SecondaryFooter/>
+      <div class="container container-center">
+        <LeafletMap location={currentLocation} />
+      </div>
+      <Reviews />
+      <SecondaryFooter />
     </>
   );
 });
